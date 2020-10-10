c4e0588f0ced67dfc323a389474b99f6ec065072
Qenawi <aldmanmero@yahoo.com>
2020-10-10 12:13:20 +0200

    init

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    @IBOutlet weak var playerOne: UIImageView!
+    @IBOutlet weak var playerTwo: UIImageView!
+    private let dice = [#imageLiteral(resourceName: "DiceOne") ,#imageLiteral(resourceName: "DiceTwo") ,#imageLiteral(resourceName: "DiceThree") ,#imageLiteral(resourceName: "DiceFour") ,#imageLiteral(resourceName: "DiceFive") ,#imageLiteral(resourceName: "DiceSix")]
+    private var imageOneAsset = 0
+    private var imageTwoAsset = 1
+    
+    
+    @IBAction func onRoleClick(_ sender: Any)
+    {
+        imageOneAsset = Int.random(in: 0...5)
+        imageTwoAsset = Int.random(in: 0...5)
+        playerOne.image = dice[imageOneAsset]
+        playerTwo.image = dice[imageTwoAsset]
 
+    }
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+    }
+   
+    
 
 }
 
